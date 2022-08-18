2d587efe2c2fc16ec980aef4cd0ec669ed3e76c6
imevanc <charal.vag@gmail.com>
2022-08-18 15:01:22 +0100

    higher-order-function-map pre video

---
+++ higher-order-functions/map/index.js
@@ -0,0 +1,7 @@
+const db = require("./../db/users.json");
+const users = db.users;
+
+// Question 1: Return an array of names
+
+// Questions 2: Return an array of objects.
+// Each object needs to have a name and a house property.
